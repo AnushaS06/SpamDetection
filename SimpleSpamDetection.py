e6a9d4629f44bee557b829e275aa9fb93e06b726
Anusha S <40312715+AnushaS06@users.noreply.github.com>
2019-02-11 19:11:31 +0530

    Create SimpleSpamDetection.py
    This code uses Multinomial Naive Bayes , AdaBoost, Logistic Regression classification techniques. The output is the accuracy of each model used.

---
+++ SimpleSpamDetection.py
@@ -0,0 +1,28 @@
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.linear_model import LogisticRegression
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('spambase/spambase.data').values
+np.random.shuffle(data) 
+
+X = data[:,:48]
+Y = data[:,-1]
+
+Xtrain = X[:-100,]
+Ytrain = Y[:-100,]
+Xtest = X[-100:,]
+Ytest = Y[-100:,]
+
+model = MultinomialNB()
+model.fit(Xtrain, Ytrain)
+print("Classification rate for NB:", model.score(Xtest, Ytest))
+
+model = AdaBoostClassifier()
+model.fit(Xtrain, Ytrain)
+print("Classification rate for AdaBoost:", model.score(Xtest, Ytest))
+
+model = LogisticRegression()
+model.fit(Xtrain, Ytrain)
+print("Classification rate for LogisticRegression:", model.score(Xtest, Ytest))
